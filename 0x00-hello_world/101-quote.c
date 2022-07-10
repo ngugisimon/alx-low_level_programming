3eec331175892e2c5f0033103847715b5fa391de
Simon Njuguna <njuguna.simon@gmail.com>
2022-07-10 03:47:12 -0700

    #2 C program that prints exactly

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-
+#include <unistd.h>
 
 /**
  *
@@ -16,10 +16,10 @@ int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
-
-			return (1);
+	return (1);
 
 }
